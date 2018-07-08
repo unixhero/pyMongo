918a4bf23a19ecc3c700320facc277f16d4a9af8
unixhero <c.friedel@outlook.com>
2018-07-08 22:55:52 +0200

    MainWindow added + WindowIcon

--- init.py
+++ init.py
@@ -1,16 +1,52 @@
+import sys
 from Connection import *
-from tkinter import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-root = Tk()
 
-label = Label(root, text="Überschrift")
+class WindowClient(QMainWindow):
+    def __init__(self, window_title='WindowClient', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initialisation(window_title)
 
-root.mainloop()
+    def initialisation(self, window_title):
+        self.setWindowTitle(window_title)
+        self.setGeometry(400, 200, 1000, 500)
+        self.setWindowIcon(QIcon('icon.png'))
+        insert = QAction(QIcon(), 'Insert', self)
+        get = QAction(QIcon(), 'Get', self)
+        update = QAction(QIcon(), 'Update', self)
+        delete = QAction(QIcon(), 'Delete', self)
+        navigation = self.addToolBar('Action')
+        navigation.addAction(get)
+        navigation.addAction(insert)
+        navigation.addAction(update)
+        navigation.addAction(delete)
+        self.form_get()
 
-# GUI tkinter
-# .....
-# Live-DB
-#
+        self.show()
 
-db = Connection()
-db.getdocuments('Trump')
+    def form_get(self):
+        label = QLabel('Get', self)
+        layout = QGridLayout()
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(label, 0, 1)
+        layout.addWidget(label, 0, 2)
+        layout.addWidget(label, 0, 3)
+        layout.addWidget(label, 1, 0)
+        layout.addWidget(label, 1, 1)
+        layout.addWidget(label, 1, 2)
+        layout.addWidget(label, 1, 3)
+
+        blah = QLayout()
+
+        self.setLayout()
+
+
+app = QApplication(sys.argv)
+w = WindowClient()
+sys.exit(app.exec_())
+
+# db = Connection()
+# db.getdocuments('Trump')
